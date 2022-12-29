57dc6e9d4e623617a4034222c376151cb0d5cfe0
NikolayNS <dmitrenko.ns@gmail.com>
2022-12-29 15:05:24 -0600

    MergeSortedArray

--- src/main/java/com/dmitrenko/leetcode/problems/resolve/MergeSortedArray.java
+++ src/main/java/com/dmitrenko/leetcode/problems/resolve/MergeSortedArray.java
@@ -1,8 +1,19 @@
 package com.dmitrenko.leetcode.problems.resolve;
 
+import java.util.Arrays;
+
 public class MergeSortedArray {
 
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int j = 0;
+        for (int i = m; i <= nums1.length - 1; i++) {
+            nums1[i] = nums2[j];
+            j++;
+        }
+        Arrays.sort(nums1);
+    }
 
+    public static void main(String[] args) {
+        new MergeSortedArray().merge(new int[]{1,2,3,0,0,0}, 3, new int[]{2,5,6}, 3);
     }
 }
